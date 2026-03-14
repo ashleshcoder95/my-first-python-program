c41469ea92f554073e06f2672cf2e011b4f67ca4
ashleshcoder95 <ashleshsharmaji95@gmail.com>
2026-03-14 22:17:30 +0530

    Create day 2 global_asset.py

---
+++ day 2 global_asset.py
@@ -0,0 +1,12 @@
+#traffic signal light program
+
+light=input("colour:")
+
+if(light=="red"):
+    print("stop please")
+elif(light=="yellow"):
+    print("wait please")
+elif(light=="green"):
+    print("go")
+else:
+    print("ligt is broken")
